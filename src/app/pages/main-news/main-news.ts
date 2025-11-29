e195dde6869fc5b2475792ccbe1ce332e3d0ea89
Marina-Banov <marina.banov@uniri.hr>
2025-11-29 12:37:58 +0100

    add news data

--- src/app/pages/main-news/main-news.ts
+++ src/app/pages/main-news/main-news.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
@@ -7,58 +7,28 @@ import { OrderListModule } from 'primeng/orderlist';
 import { PickListModule } from 'primeng/picklist';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { TagModule } from 'primeng/tag';
-import { Product, ProductService } from '../service/product.service';
+import { Data } from '@/services/data';
+import { News } from '@/pages/service/news.service';
 
 @Component({
     selector: 'app-main-news',
     standalone: true,
     imports: [CommonModule, DataViewModule, FormsModule, SelectButtonModule, PickListModule, OrderListModule, TagModule, ButtonModule],
     templateUrl: './main-news.html',
-    styleUrl: './main-news.scss',
-    providers: [ProductService]
+    styleUrl: './main-news.scss'
 })
 export class MainNews {
-    layout: 'list' | 'grid' = 'list';
+    news: News[] = [];
 
-    options = ['list', 'grid'];
+    private dataService = inject(Data);
 
-    products: Product[] = [];
-
-    sourceCities: any[] = [];
-
-    targetCities: any[] = [];
-
-    orderCities: any[] = [];
-
-    constructor(private productService: ProductService) {}
+    constructor() {}
 
     ngOnInit() {
-        this.productService.getProductsSmall().then((data) => (this.products = data.slice(0, 6)));
-
-        this.sourceCities = [
-            { name: 'San Francisco', code: 'SF' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Paris', code: 'PRS' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Berlin', code: 'BRL' },
-            { name: 'Barcelona', code: 'BRC' },
-            { name: 'Rome', code: 'RM' }
-        ];
-
-        this.targetCities = [];
-
-        this.orderCities = [
-            { name: 'San Francisco', code: 'SF' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Paris', code: 'PRS' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Berlin', code: 'BRL' },
-            { name: 'Barcelona', code: 'BRC' },
-            { name: 'Rome', code: 'RM' }
-        ];
+        this.dataService.loadData('main-news').subscribe((data) => (this.news = data));
     }
 
-    getSeverity(product: Product) {
+    getSeverity(product: News) {
         switch (product.inventoryStatus) {
             case 'INSTOCK':
                 return 'success';
