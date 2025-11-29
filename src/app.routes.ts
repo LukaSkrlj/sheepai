4a2715a604259070d49969b9588e679fd29e9bd1
Marina-Banov <marina.banov@uniri.hr>
2025-11-29 17:41:37 +0100

    reels
    # Conflicts:
    #	src/app.routes.ts

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -10,6 +10,7 @@ import {ListTopic} from "@/list-topic/list-topic";
 import { Articles } from './app/pages/articles/articles';
 import { ArticleDemo } from './app/pages/article-demo/article-demo';
 import {Notes} from "@/pages/notes/notes";
+import { Reels } from './app/pages/reels/reels';
 
 export const appRoutes: Routes = [
     {
@@ -23,6 +24,9 @@ export const appRoutes: Routes = [
             { path: 'all-topics', component: ListTopic },
             { path: 'articles', component: Articles },
             { path: 'article/:id', component: ArticleDemo },
+            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
+            { path: 'reels', component: Reels },
             { path: 'notes', component: Notes },
         ]
     },
---
+++ src/app/pages/reels/reels.ts
@@ -0,0 +1,42 @@
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+class Video {
+    item: string;
+    isPlaying: boolean;
+}
+
+@Component({
+    selector: 'app-reels',
+    imports: [CommonModule],
+    templateUrl: './reels.html',
+    styleUrl: './reels.scss'
+})
+export class Reels {
+    items: Video[] = [
+        { item: 'reel1', isPlaying: false },
+        { item: 'reel2', isPlaying: false },
+        { item: 'reel3', isPlaying: false }
+    ];
+    items2: Video[] = [
+        { item: 'reel3', isPlaying: false },
+        { item: 'reel1', isPlaying: false },
+        { item: 'reel2', isPlaying: false }
+    ];
+
+    @ViewChildren('video') videos!: QueryList<ElementRef<HTMLVideoElement>>;
+
+    playVideo(event: Event) {
+        const video = event.target as HTMLVideoElement;
+        video.muted = true; // REQUIRED for hover play
+        video.playsInline = true;
+        video.play().catch((err) => {
+            console.warn('Hover play blocked:', err);
+        });
+    }
+
+    pauseVideo(event: Event) {
+        const video = event.target as HTMLVideoElement;
+        video.pause();
+    }
+}
